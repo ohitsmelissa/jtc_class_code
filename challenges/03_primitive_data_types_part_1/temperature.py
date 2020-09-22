f6d7598add6553b89e3c5ac6dd964d5853d6d206
Melissa Bautista <melissa.bautista@baruchmail.cuny.edu>
2020-09-22 19:54:01 -0400

    adding temperature.py challenge

---
+++ challenges/03_primitive_data_types_part_1/temperature.py
@@ -0,0 +1,25 @@
+#!
+celsius_100 = 100.0  #conversting 100 degrees fahrnheit INTO celcius 
+new_celsiuss = (celsius_100-32)*(5/9)
+print(new_celsiuss)
+
+#2
+celsius_0 = 0.0   #farenheight is 0 ane next line we converting F TO celcius 
+new_celsius = (celsius_0-32)*(5/9)
+print(new_celsius)
+
+
+# Convert a temperature of 34.2 degrees fahrenheit 	TO celsius
+# Do this one all in one print statement without saving any variables
+print((34.2-32)*(5/9))  
+
+#converting CELSIUS TO FAHRENHEIGHT 
+
+print((5*9/5)+32) # mutilplt by 9/5 lol and jsut add 32 (work backwards)
+
+#5 
+convertedCel= (30.2*9/5)+32
+print(convertedCel,"F") # this one is hotter, its 86.36
+print("85.1 degrees F")  #this one is 85.1
+
+
